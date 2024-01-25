84f330d5660d2210f6df417cc3aae551c4df091d
osc <oscbusiness@hotmail.com>
2024-01-25 07:58:06 -0500

    Update c_framework.lua

--- client/c_framework.lua
+++ client/c_framework.lua
@@ -1,5 +1,5 @@
-if Config.Framework == 'esx' then
+if Config.Framework == 'esx' then -- check config
     ESX = exports['es_extended']:getSharedObject()
-elseif Config.Framework == 'qb' then
+elseif Config.Framework == 'qb' then -- check config
     QBCore = exports['qb-core']:GetCoreObject()
 end
